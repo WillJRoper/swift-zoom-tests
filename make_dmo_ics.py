5be201507bbc2f173bf64562a21dee8629a6c0d0
wjr21 <wjr21@sussex.ac.uk>
2024-04-20 21:19:18 +0200

    More fixes

--- make_dmo_ics.py
+++ make_dmo_ics.py
@@ -82,7 +82,7 @@ def carve_out_region(pos, masses, vels, region_rad, max_pos, boxsize):
     """
     # Shift positions to centre on max_pos
     pos -= max_pos
-    pos = (pos + boxsize) % boxsize
+    pos = (pos + boxsize / 2) % boxsize
 
     # Mask out particles outside the region_rad from max_pos
     print(f"Carving out region with radius {region_rad} Mpc.")
