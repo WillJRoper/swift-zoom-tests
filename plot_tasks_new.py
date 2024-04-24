bcb7fb983c99ad985d3cb37c90e48c94bdece7c0
wjr21 <wjr21@sussex.ac.uk>
2024-04-24 09:54:22 +0200

    Generalised the plotting function

--- plot_tasks_new.py
+++ plot_tasks_new.py
@@ -30,57 +30,104 @@ non_periodic_tests = [
     "geometric_nonperiodic_tasks",
 ]
 
-# Parse all the task files
-runs = {}
-for branch in branches:
-    for test in non_periodic_tests:
-        runs[branch + "/" + test] = TaskParser(
-            f"{base}/{branch}/{test}/thread_info-step64.dat"
+
+def make_task_hist(cell_type=None, cell_subtype=None, depth=None):
+    # Parse all the task files
+    runs = {}
+    for branch in branches:
+        for test in non_periodic_tests:
+            runs[branch + "/" + test] = TaskParser(
+                f"{base}/{branch}/{test}/thread_info-step64.dat"
+            )
+            print(branch, test, runs[branch + "/" + test].get_tasks()[-1])
+
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(111)
+    ax.set_xscale("log")
+    ax.grid(True)
+
+    for i, (name, run) in enumerate(runs.items()):
+        mask = np.ones(len(run.task_labels), dtype=bool)
+        if cell_type is not None:
+            mask = np.logical_and(
+                mask,
+                np.logical_or(
+                    run.ci_types == cell_type,
+                    run.cj_types == cell_type,
+                ),
+            )
+        if cell_subtype is not None:
+            mask = np.logical_and(
+                mask,
+                np.logical_or(
+                    run.ci_subtypes == cell_subtype,
+                    run.cj_subtypes == cell_subtype,
+                ),
+            )
+        if depth is not None:
+            mask = np.logical_and(
+                mask,
+                np.logical_or(run.ci_depths == depth, run.cj_depths == depth),
+            )
+
+        labels, counts = np.unique(run.task_labels[mask], return_counts=True)
+
+        # Sort the labels and counts by counts in descending order
+        sorted_indices = np.argsort(-counts)
+        labels = labels[sorted_indices]
+        counts = counts[sorted_indices]
+
+        # Calculate positions for horizontal bars
+        positions = np.arange(len(labels))
+
+        # Compute the width between labels
+        width = 0.8 / (len(runs) + 1)
+
+        # Create horizontal bar plot
+        bars = ax.barh(
+            positions + (i * width),
+            counts,
+            height=0.75 / len(runs),
+            label=name,
+            alpha=0.7,
         )
-        print(branch, test, runs[branch + "/" + test].get_tasks()[-1])
 
-fig = plt.figure(figsize=(12, 6))
-ax = fig.add_subplot(111)
-ax.set_xscale("log")
-ax.grid(True)
+        if "long_range" in name:
+            # Adding hatching
+            for bar in bars:
+                bar.set_hatch("//")
+                bar.set_edgecolor(bar.get_facecolor())
 
-for i, (name, run) in enumerate(runs.items()):
-    labels, counts = np.unique(run.task_labels, return_counts=True)
+    ax.set_yticks(np.arange(len(labels)) + 0.2)
+    ax.set_yticklabels(labels)
+    ax.invert_yaxis()
 
-    # Sort the labels and counts by counts in descending order
-    sorted_indices = np.argsort(-counts)
-    labels = labels[sorted_indices]
-    counts = counts[sorted_indices]
+    ax.set_xlabel("Count")
 
-    # Calculate positions for horizontal bars
-    positions = np.arange(len(labels))
+    # Place the legend at the bottom of the plot
+    ax.legend(loc="upper center", bbox_to_anchor=(0.5, -0.15), ncol=3)
 
-    # Compute the width between labels
-    width = 0.8 / (len(runs) + 1)
+    # Define the filename
+    filename = "plots/task_count_comp"
+    if cell_type is not None:
+        filename += f"_type{cell_type}"
+    if cell_subtype is not None:
+        filename += f"_subtype{cell_subtype}"
+    if depth is not None:
+        filename += f"_depth{depth}"
 
-    # Create horizontal bar plot
-    bars = ax.barh(
-        positions + (i * width),
-        counts,
-        height=0.75 / len(runs),
-        label=name,
-        alpha=0.7,
-    )
+    fig.tight_layout()
+    fig.savefig(filename, bbox_inches="tight")
 
-    if "long_range" in name:
-        # Adding hatching
-        for bar in bars:
-            bar.set_hatch("//")
-            bar.set_edgecolor(bar.get_facecolor())
 
-ax.set_yticks(np.arange(len(labels)) + 0.2)
-ax.set_yticklabels(labels)
-ax.invert_yaxis()
-
-ax.set_xlabel("Count")
-
-# Place the legend at the bottom of the plot
-ax.legend(loc="upper center", bbox_to_anchor=(0.5, -0.15), ncol=3)
-
-fig.tight_layout()
-fig.savefig("plots/task_count_comp_horizontal.png", bbox_inches="tight")
+make_task_hist()
+make_task_hist(cell_type=1)
+make_task_hist(cell_type=2)
+make_task_hist(cell_type=3)
+make_task_hist(cell_subtype=1)
+make_task_hist(cell_subtype=2)
+make_task_hist(depth=0)
+make_task_hist(depth=1)
+make_task_hist(depth=2)
+make_task_hist(depth=3)
+make_task_hist(depth=4)
